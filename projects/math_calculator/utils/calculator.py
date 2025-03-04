bdf64ef5011f76426540ac96cd30a3c48c014687
Chris <kaychrisharry@gmail.com>
2025-03-04 16:06:53 +0530

    done w calc

--- projects/math_calculator/utils/calculator.py
+++ projects/math_calculator/utils/calculator.py
@@ -1,4 +1,43 @@
 # evaluate the expression
 
 def calculator(exp):
-    pass
+    # convert infix to postfix
+    stk = []
+    postfix = ''
+    for x in exp:
+        if x.isdigit():
+            postfix += x
+        elif x in ('*', '/', '-', '+'):
+            if not stk or x > stk[-1]:
+                stk.append(x)
+            else:
+                while stk and stk[-1] >= x:
+                    postfix += stk[-1]
+                    stk.pop()
+                stk.append(x)
+        elif x == '(':
+            stk.append('(')
+        elif x == ')':
+            while stk and stk[-1] != '(':
+                postfix += stk[-1]
+                stk.pop()
+            stk.pop()
+    for x in stk: postfix += x
+    
+    # evaluating the postfix expression
+    stk = []
+    ops = '*', '/', '-', '+'
+    for x in postfix:
+        if x.isdigit():
+            stk.append(int(x))
+        elif x in ops:
+            X, Y = int(stk[-2]), int(stk[-1])
+            stk = stk[:-2]
+            if x == '+': stk.append(X+Y)
+            if x == '-': stk.append(X-Y)
+            if x == '/': stk.append(X/Y)
+            if x == '*': stk.append(X*Y)
+
+    return stk[0]
+
+print(calculator('2 + 3 * (4 - 1)'))
