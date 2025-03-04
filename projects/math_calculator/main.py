95a91db4862ff8f37a50a39bd834cef69e276f77
Chris <kaychrisharry@gmail.com>
2025-03-04 17:07:21 +0530

    changed the date format for logging

--- projects/math_calculator/main.py
+++ projects/math_calculator/main.py
@@ -7,24 +7,24 @@ from utils import calculator
 logging.basicConfig(filename='logs/app.log', level=logging.DEBUG)
 
 try:
-    today = datetime.today()
+    today = datetime.now()
     print('Chris\'s Mathematical Expression Validator &')
     print('Enter your expression or \'exit\' to quit > ', end='')
     expr = input()
     is_valid = validator.is_valid_expression(expr)
     if is_valid:
         ans = calculator.calculator(expr)
-        logging.info(f'{today} - INFO - Valid expression: {expr}')
+        logging.info(f'{today:%Y-%m-%d %H:%M:%S} - INFO - Valid expression: {expr}')
         print(f'The result is: {ans}')
-        logging.info(f'{today} - INFO - Result of the expression is: {ans}')
-        logging.info(f'{today} - INFO - Exiting program')
+        logging.info(f'{today:%Y-%m-%d %H:%M:%S} - INFO - Result of the expression is: {ans}')
+        logging.info(f'{today:%Y-%m-%d %H:%M:%S} - INFO - Exiting program')
     elif not is_valid:
-        logging.error(f'{today} - ERROR - Invalid expression!')
-        logging.info(f'{today} - INFO - Exiting program')
+        logging.error(f'{today:%Y-%m-%d %H:%M:%S} - ERROR - Invalid expression!')
+        logging.info(f'{today:%Y-%m-%d %H:%M:%S} - INFO - Exiting program')
         print('Invalid expression')
     elif expr == 'exit':
         print('bye bye!')
-        logging.info(f'{today} - INFO - Exiting program')
+        logging.info(f'{today:%Y-%m-%d %H:%M:%S} - INFO - Exiting program')
         sys.exit(0)
     else:
         print('The expression is not valid.')
