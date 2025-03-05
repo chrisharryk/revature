989156c7c211eba703d135bd162d5bf29a61a22f
Chris <kaychrisharry@gmail.com>
2025-03-05 08:16:31 +0530

    made changes to calc, managing edge cases

--- projects/math_calculator/utils/calculator.py
+++ projects/math_calculator/utils/calculator.py
@@ -2,27 +2,41 @@
 
 def calculator(exp):
     # convert infix to postfix
+    # EG: 77 / (2 * 97) -> 77 2 97 * /
+    # removing brackets
+    exp = exp.split()
+    tmp = []
+    for x in exp:
+        if x[0] == '(': 
+            tmp.append('(')
+            tmp.append(x[1:])
+        elif x[-1] == ')':
+            tmp.append(x[:-1])
+            tmp.append(')')
+        else: tmp.append(x)
+    exp = [x for x in tmp if x]
+    
     stk = []
-    postfix = ''
+    postfix = []
     for x in exp:
         if x.isdigit():
-            postfix += x
+            postfix.append(x)
         elif x in ('*', '/', '-', '+'):
             if not stk or x > stk[-1]:
                 stk.append(x)
             else:
                 while stk and stk[-1] >= x:
-                    postfix += stk[-1]
+                    postfix.append(stk[-1])
                     stk.pop()
                 stk.append(x)
         elif x == '(':
             stk.append('(')
         elif x == ')':
             while stk and stk[-1] != '(':
-                postfix += stk[-1]
+                postfix.append(stk[-1])
                 stk.pop()
             stk.pop()
-    for x in stk: postfix += x
+    for x in stk: postfix.append(x)
     
     # evaluating the postfix expression
     stk = []
